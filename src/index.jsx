06821329b24d01aefcf93f8c0229d641ca3ce42f
zynkd <coding707@mailbox.org>
2022-12-09 19:28:28 +0100

    Imported some global styles for the body and utility class container

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './normalize.scss';
+import './index.scss';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

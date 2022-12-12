3db49b3201bafa11929dbbc9e7e8ebb0006c784c
zynkd <coding707@mailbox.org>
2022-12-12 18:22:21 +0100

    Added `uuid` pkg. to generate unique ID for each new feedback items

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import Header from './components/Header/Header';
 import FeedbackList from './components/FeedbackList/FeedbackList';

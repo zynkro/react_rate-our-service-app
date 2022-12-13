7faa62fff2d06448705e797ca2b87e0103409b43
zynkd <coding707@mailbox.org>
2022-12-13 09:45:42 +0100

    Wrapped everything in Router, added Route to '/about' and '/'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import FeedbackList from './components/FeedbackList/FeedbackList';
 import FeedbackStats from './components/FeedbackStats/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm/FeedbackForm';
+import AboutPage from './components/pages/AboutPage/AboutPage';
+import AboutIconLink from './components/AboutIconLink/AboutIconLink';
 
 import feedbackData from './data/feedbackData';
 
@@ -23,15 +26,32 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header title='Rate Our Service' />
 
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedbacks={feedbacks} />
-        <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedbacks={feedbacks} />
+                <FeedbackList
+                  feedbacks={feedbacks}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+
+      <AboutIconLink />
+    </Router>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.scss';
 
-const Header = ( { title } ) => (
+const Header = ({ title }) => (
   <header className='header'>
-    <h1 className="header__title">{title}</h1>
+    <Link to='/'>
+      <h1 className='header__title'>{title}</h1>
+    </Link>
   </header>
 );
 

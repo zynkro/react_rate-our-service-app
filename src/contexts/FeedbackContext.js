9818f0767e420fbc73672920dba3b1d57695d087
zynkd <coding707@mailbox.org>
2022-12-14 17:00:07 +0100

    Updated the rest of the functionality to make calls to JSON-server

--- src/contexts/FeedbackContext.js
+++ src/contexts/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
@@ -25,22 +24,30 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch all feedbacks
   const fetchFeedbacks = async () => {
-    const response = await fetch(
-      '/feedbacks?_sort=id&_order=desc',
-    );
+    const response = await fetch('/feedbacks?_sort=id&_order=desc');
     const data = await response.json();
 
     setFeedbacks(data);
     setIsLoading(false);
   };
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedbacks([newFeedback, ...feedbacks]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedbacks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedbacks([data, ...feedbacks]);
   };
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (confirm('Are you sure?')) {
+      await fetch(`/feedbacks/${id}`, { method: 'DELETE' });
       setFeedbacks(feedbacks.filter((feedback) => feedback.id !== id));
     }
   };
@@ -55,13 +62,23 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Used when feedbackEditState.editMode is true
-  const updateFeedback = (id, updatedFeedback) => {
+  const updateFeedback = async (id, updatedFeedback) => {
+    const response = await fetch(`/feedbacks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedFeedback),
+    });
+
+    const data = await response.json();
+
     setFeedbacks(
       feedbacks.map((feedback) =>
         feedback.id === id
           ? {
               ...feedback,
-              ...updatedFeedback,
+              ...data,
             }
           : feedback,
       ),
